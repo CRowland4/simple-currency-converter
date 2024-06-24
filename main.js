2e6396b5af03f5c583ebf71c6e18b3600f7be74d
Caleb Rowland <cjrowland4@gmail.com>
2024-06-24 08:32:57 -0500

    Add stage 2

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
 const input = require('sync-input');
 
-
 function main() {
+    const oneUSD = {
+        USD: 1,
+        JPY: 113.5,
+        EUR: 0.89,
+        RUB: 74.36,
+        GBP: 0.75
+    }
     console.log("Welcome to Currency Converter!");
-    console.log("1 USD equals 1 USD");
-    console.log("1 USD equals 113.5 JPY");
-    console.log("1 USD equals 0.89 EUR");
-    console.log("1 USD equals 74.36 RUB");
-    console.log("1 USD equals 0.75 GBP");
+    for (const [currency, rate] of Object.entries(oneUSD)) {
+        console.log(`1 USD equals ${rate} ${currency}`);
+    }
+
+    console.log("I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP");
+    doConversion(oneUSD);
+
+}
+
+
+function doConversion(oneUSD) {
+    console.log("Type the currency you wish to convert: USD");
+    const to = input("To: ").toUpperCase();
+    if (!Object.keys(oneUSD).includes(to)) {
+        console.log("Unknown currency");
+        return
+    }
+
+    const amount = input("Amount: ");
+    if (amount < 1) {
+        console.log("The amount cannot be less than 1");
+        return
+    } else if (isNaN(Number(amount))) {
+        console.log("The amount has to be a number");
+        return
+    }
+
+    console.log(`Result: ${amount} USD equals ${(oneUSD[to] * amount).toFixed(4)} ${to}`);
 }
 
 
 main();
+
